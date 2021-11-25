bd22b305d12459289b0e57e66cdb285137ee14e6
Maxime <emixampons@gmail.com>
2021-11-25 19:24:18 +0100

    initial commit

---
+++ home.nix
@@ -0,0 +1,59 @@
+
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "maxime";
+  home.homeDirectory = "/home/maxime";
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+  home.packages = with pkgs; [ 
+    # Entertainment
+    discord
+    spotify
+    # Low-Level
+    gcc
+    # Web
+    nodejs
+    # Haskell
+    ghc
+    cabal-install
+    stack
+    haskell-language-server
+  ];
+
+
+  programs.zsh = {
+    enable = true;
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+      ];
+    };
+    initExtra = ''
+      alias nedo=sudo
+      source $HOME/.p10k.zsh
+      export PATH=$HOME/.ghcup/bin/:$HOME/.velle/:$PATH
+    '';
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
+
